f0f4e869bb942b634243b7cd6a2a790ee59d9f12
Jaimie Zhu <jaimiezhu@jaimiezhu-mbp.local>
2015-06-23 14:19:40 -0700

    Initial Commit

---
+++ Colorboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Colorboard
+//
+//  Created by Jaimie Zhu on 6/23/15.
+//  Copyright (c) 2015 Jaimie Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Colorboard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Colorboard
+//
+//  Created by Jaimie Zhu on 6/23/15.
+//  Copyright (c) 2015 Jaimie Zhu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
